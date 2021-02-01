e55b4dfb3af812665dfae81eb64fe4cf46daa0cb
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-02-01 13:32:47 -0800

    editing to try to fix server errors

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 
+
 module.exports = {
-  "migrationDirectory": "migrations",
-  "driver": "pg",
-  connectionString:
-		process.env.DATABASE_URL || 'postgres://pciywrubfjgbyc:41b4709df73f13ae4a8928a6d479fcf4a9dd2aed21e6b25de7573f63c05d90d2@ec2-3-211-149-196.compute-1.amazonaws.com:5432/d7pe60vj9vkohl'
-}
+  migrationDirectory: "migrations",
+  driver: "pg",
+  host: process.env.MIGRATION_DATABASE_HOST,
+  port: process.env.MIGRATION_DATABASE_PORT,
+  database: process.env.MIGRATION_DATABASE_NAME,
+  username: process.env.MIGRATION_DATABASE_USER,
+  password: process.env.MIGRATION_DATABASE_PASS,
+};
